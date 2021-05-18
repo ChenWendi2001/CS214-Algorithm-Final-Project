31d3d7c3b56629514a499b834a9c3366c0054c75
cwher <cwher@outlook.com>
2021-05-18 15:52:28 +0800

    feat: add debug functions

--- common.hpp
+++ common.hpp
@@ -5,8 +5,15 @@
 #include <string>
 #include <utility>
 #include <unordered_map>
+#include <queue>
+#include <fstream>
+#include <cstdlib>
+#include <iostream>
+#include <iomanip>
+#include <cmath>
 
 using std::pair;
+using std::priority_queue;
 using std::string;
 using std::unordered_map;
 using std::vector;
@@ -26,6 +33,36 @@ struct Graph
     // e.g. slots["DC1"]={2,{"tA1"}}
     //  the capacity of DC1 is 2 and tA1 is running
     unordered_map<string, pair<int, vector<string>>> slots;
+
+    void printStatus()
+    {
+        for (const auto &DC : slots)
+        {
+            std::cout << std::setw(4)
+                      << DC.first << '|';
+            const auto tasks = DC.second.second;
+            for (const auto &task : tasks)
+                std::cout << std::setw(4)
+                          << task << '|';
+            for (int i = 0; i < DC.second.first - tasks.size(); ++i)
+                std::cout << std::setw(4)
+                          << ' ' << '|';
+            std::cout << std::endl;
+        }
+    }
 };
+
+// print error message and terminate
+void printError(string msg)
+{
+    std::cout << "Error: " << msg << std::endl;
+    std::exit(0);
+}
+
+// print warning message
+void printWarning(string msg)
+{
+    std::cout << "Warning: " << msg << std::endl;
+}
 
 #endif
