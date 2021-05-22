9a87d05aff1f5b09ee0df9dd79da96bc2450f54f
cwher <cwher@outlook.com>
2021-05-22 19:08:49 +0800

    refactor: move init_data to common and refactor some code inside each class

--- main_greedy.cpp
+++ main_greedy.cpp
@@ -1,25 +1,20 @@
 #include "common.hpp"
-#include "json.hpp"
-#include <fstream>
 #include "DAG.hpp"
 #include "scheduler.hpp"
 #include "simulator.hpp"
 
-using json = nlohmann::json;
-
-shared_ptr<Graph> graph;
-
-void init_data();
-
 int main()
 {
-    init_data();
+    shared_ptr<Graph> graph;
+    graph = make_shared<Graph>();
+    init_data(graph);
     DAG dag;
-    Scheduler scheduler(graph);
+    Scheduler scheduler;
     scheduler.sched_type = scheduler.GREEDY;
     Simulator sim;
 
     dag.init(graph);
+    scheduler.initGraph(graph);
     sim.updateGraph(graph);
     sim.printStatus();
     int task_cnt = 0;
@@ -43,160 +38,7 @@ int main()
         dag.updateDAG(finished);
     }
     std::cout << sim.getTime() << "\n";
-
     graph->printFinishTime();
 
-    //Debug code
-    // vector<string> temp = dag.getSubmit();
-    // for(auto iter = temp.begin();iter!=temp.end();iter++){
-    //     std::cout<<*iter<<" ";
-    // }
     return 0;
-}
-
-void init_data()
-{
-    graph = graph = make_shared<Graph>();
-    //
-    //initailize constraint
-    //
-    json constraint;
-    std::ifstream constraint_file("./ToyData/constraint.json");
-    constraint_file >> constraint;
-    for (auto iter = constraint["constraint"].begin(); iter != constraint["constraint"].end(); iter++)
-    {
-        graph->constraint.push_back(std::make_pair<string, string>((*iter)["start"], (*iter)["end"]));
-    }
-
-    //Debug code
-    // for(auto iter = graph->constraint.begin();iter!=graph->constraint.end();iter++){
-    //     std::cout<<(*iter).first<<" "<<(*iter).second<<"\n";
-    // }
-
-    //
-    //initialize run_time, require and job_task, which job
-    //
-    json job;
-    std::ifstream job_file("./ToyData/job_list.json");
-    job_file >> job;
-
-    int num_of_jobs = job["job"].size();
-    for (int i = 0; i < num_of_jobs; ++i)
-    {
-        int num_of_task = job["job"][i]["task"].size();
-        unordered_set<string> &task = graph->job_task[job["job"][i]["name"]];
-        for (int j = 0; j < num_of_task; ++j)
-        {
-            string task_name = job["job"][i]["task"][j]["name"];
-            //run time
-            graph->run_time[task_name] = job["job"][i]["task"][j]["time"];
-            //job list
-            task.insert(task_name);
-            //which job
-            graph->which_job[task_name] = job["job"][i]["name"];
-            int num_of_resource = job["job"][i]["task"][j]["resource"].size();
-            for (int k = 0; k < num_of_resource; ++k)
-            {
-                graph->require[job["job"][i]["task"][j]["name"]].push_back(
-                    std::make_pair(job["job"][i]["task"][j]["resource"][k]["name"], job["job"][i]["task"][j]["resource"][k]["size"]));
-            }
-        }
-    }
-
-    //Debug code
-
-    // for(int i = 0;i<num_of_jobs;++i){
-    //     int num_of_task = job["job"][i]["task"].size();
-    //     for(int j = 0;j<num_of_task;++j){
-    //         std::cout<<job["job"][i]["task"][j]["name"]<<":" <<graph->run_time[job["job"][i]["task"][j]["name"]];
-    //         std::cout<<"\n";
-    //         int num_of_resource = job["job"][i]["task"][j]["resource"].size();
-    //         for(int k = 0;k<num_of_resource;++k){
-    //             for(auto iter = graph->require[job["job"][i]["task"][j]["name"]].begin();iter !=graph->require[job["job"][i]["task"][j]["name"]].end();iter++)
-    //                 std::cout<<(*iter).first<<" "<<(*iter).second<<"\n";
-    //         }
-    //     }
-    // }
-
-    //
-    //initialize graph->resources
-    //
-    json DC;
-    std::ifstream DC_file("./ToyData/DC.json");
-    DC_file >> DC;
-
-    int num_of_dc = DC["DC"].size();
-    for (int i = 0; i < num_of_dc; ++i)
-    {
-        int num_of_resource = DC["DC"][i]["data"].size();
-        for (int j = 0; j < num_of_resource; ++j)
-            graph->resource_loc[DC["DC"][i]["data"][j]] = DC["DC"][i]["name"];
-    }
-
-    //Debug code
-
-    // for(int i = 0;i<num_of_dc;++i){
-    //     int num_of_resource = DC["DC"][i]["data"].size();
-    //     for(int j = 0;j<num_of_resource;++j)
-    //         std::cout<<DC["DC"][i]["data"][j]<<":"<<graph->resource_loc[DC["DC"][i]["data"][j]]<<'\n';
-    // }
-
-    //
-    //initialize edges
-    //
-
-    json link;
-    std::ifstream link_file("./ToyData/link.json");
-    link_file >> link;
-
-    for (int i = 0; i < num_of_dc; ++i)
-    {
-        for (int j = 0; j < num_of_dc; ++j)
-        {
-            if (link["link"][i]["bandwidth"][j] == -1)
-                graph->edges[link["link"][i]["start"]][link["link"][j]["start"]] = 0xFFFF;
-            else
-                graph->edges[link["link"][i]["start"]][link["link"][j]["start"]] = 1 / double(link["link"][i]["bandwidth"][j]);
-        }
-    }
-
-    //Floyd
-    for (int i = 0; i < num_of_dc; ++i)
-    {
-        for (int j = 0; j < num_of_dc; ++j)
-        {
-            for (int k = 0; k < num_of_dc; ++k)
-            {
-                auto dc_i = DC["DC"][i]["name"];
-                auto dc_j = DC["DC"][j]["name"];
-                auto dc_k = DC["DC"][k]["name"];
-                if (graph->edges[dc_i][dc_j] > graph->edges[dc_i][dc_k] + graph->edges[dc_k][dc_j])
-                {
-                    graph->edges[dc_i][dc_j] = graph->edges[dc_i][dc_k] + graph->edges[dc_k][dc_j];
-                }
-            }
-        }
-    }
-
-    //Debug code
-
-    // for(int i = 0;i<num_of_dc;++i){
-    //     for(int j = 0;j<num_of_dc;++j){
-    //          std::cout<<int(1/graph->edges[DC["DC"][i]["name"]][DC["DC"][j]["name"]])<<"\t";
-    //     }
-    //     std::cout<<std::endl;
-    // }
-
-    //
-    //initialize slots
-    //
-    for (int i = 0; i < num_of_dc; ++i)
-    {
-        graph->slots[DC["DC"][i]["name"]].first = DC["DC"][i]["size"];
-    }
-
-    //Debug code
-    // for(int i = 0;i<num_of_dc;++i){
-    //     std::cout<<graph->slots[DC["DC"][i]["name"]].first<<"\t";
-    // }
 }
--- scheduler.hpp
+++ scheduler.hpp
@@ -10,11 +10,11 @@ class Scheduler
 private:
     typedef pair<double, pair<string, string>> Arrange;
     // pair<transmit time(2),pair<slots(DC1),tasks(tA1)>>
-    //the graph pointer
-    shared_ptr<Graph> graph;
-    //tasks are available but have not been scheduled
 
-    class compare
+    // the graph pointer
+    shared_ptr<Graph> graph;
+
+    class ArrangeCompare
     {
     public:
         bool operator()(const Arrange &a,
@@ -23,108 +23,117 @@ private:
             return a.first > b.first;
         }
     };
+
+    // tasks are available but have not been scheduled
     unordered_set<string> ready_queue;
-    double count_time(string task_name, string which_slot)
+
+private:
+    double count_time(const string &task_name,
+                      const string &which_slot)
     {
-        vector<pair<string, double>> resource_requires =
-            (graph->require)[task_name];
-        double max = 0;
-        for (pair<string, double> resource : resource_requires)
+        vector<pair<string, double>>
+            resource_requires = graph->require[task_name];
+        double mx = 0;
+        for (const auto &resource : resource_requires)
         {
-            string resource_position = graph->resource_loc[resource.first];
-            double bandwidth = (graph->edges)[resource_position][which_slot];
+            // resource_position
+            string loc = graph->resource_loc[resource.first];
+            double bandwidth = graph->edges[loc][which_slot];
             double cost = resource.second * bandwidth;
-            if (cost > max)
-                max = cost;
+            mx = std::max(mx, cost);
         }
-        return max;
+        return mx;
     }
+
+    // assign tasks using greedy approach
     vector<Arrange> getGreedy()
     {
-        priority_queue<Arrange, vector<Arrange>, compare> mapping;
-        for (unordered_set<string>::iterator task_iter = ready_queue.begin(); task_iter != ready_queue.end(); ++task_iter)
+        priority_queue<Arrange,
+                       vector<Arrange>, ArrangeCompare>
+            Q;
+        // unordered_set<string>::iterator
+        for (const auto &task_iter : ready_queue)
         {
-            for (unordered_map<string, pair<int, unordered_set<string>>>::iterator
-                     slot = graph->slots.begin();
-                 slot != graph->slots.end(); ++slot)
+            // unordered_map<string, pair<int, unordered_set<string>>>::iterator
+            for (const auto &slot : graph->slots)
             {
-                if ((slot->second).first != 0)
-                { //capacity>0
+                if (slot.second.second.size() <
+                    slot.second.first) // capacity>0
+                {
                     Arrange this_method;
-                    this_method.first = count_time(*task_iter, slot->first);
-                    this_method.second.second = *task_iter;
-                    this_method.second.first = slot->first;
-                    mapping.push(this_method);
+                    this_method.first = count_time(task_iter, slot.first);
+                    this_method.second.second = task_iter;
+                    this_method.second.first = slot.first;
+                    Q.push(this_method);
                 }
             }
         }
         vector<Arrange> assignments;
-        Arrange assignment;
         unordered_map<string, int> used;
-        //some slots of graph arranged just now
-        while (!mapping.empty())
+        // some slots of graph arranged just now
+        while (!Q.empty())
         {
-            assignment = mapping.top();
-            mapping.pop();
+            Arrange assignment = Q.top();
+            Q.pop();
             string task = assignment.second.second;
             string DC = assignment.second.first;
 
             if (ready_queue.find(task) != ready_queue.end())
-            { //tasks in ready_queue
-                int DC_used;
-                unordered_map<string, int>::iterator f = used.find(DC);
-                if (f == used.end())
-                    DC_used = 0;
-                else
-                    DC_used = f->second;
-                if (graph->slots[DC].first > graph->slots[DC].second.size() + DC_used)
-                {
-                    //slots has capacity
+            {
+                // tasks in ready_queue
+                // int DC_used = used.find(DC) == used.end() ? 0 : used[DC];
+                // map initialize int with 0
+                int DC_used = used[DC];
+                auto &slot = graph->slots[DC];
 
-                    //arrange successfully
-                    ready_queue.erase(task);
-                    //pop from ready_queue
+                if (slot.first > slot.second.size() + DC_used)
+                {
+                    // slots has capacity
+                    // arrange successfully
+                    ready_queue.erase(task); // pop from ready_queue
                     assignments.push_back(assignment);
-                    if (f == used.end())
-                    { //DC not in used
-                        used[DC] = 1;
-                    }
-                    else
-                    {
-                        ++used[DC];
-                    }
+                    used[DC]++;
                 }
             }
         }
         return assignments;
     }
+
+    // assign tasks randomly
     vector<Arrange> getRandom()
     {
         vector<pair<string, int>> available_slot;
-        int number;
-        for (pair<string, pair<int, unordered_set<string>>> slot : graph->slots)
+        // pair<string, pair<int, unordered_set<string>>>
+        for (const auto &slot : graph->slots)
         {
-            number = slot.second.first - slot.second.second.size();
-            if (number != 0)
+            if (slot.second.second.size() > slot.second.first)
+                printError("Slot Overflow in Scheduler");
+
+            int empty_cnt = slot.second.first -
+                            slot.second.second.size();
+            if (empty_cnt != 0)
             {
-                available_slot.push_back(make_pair(slot.first, number));
+                available_slot.push_back(
+                    make_pair(slot.first, empty_cnt));
             }
         }
+
         vector<Arrange> assignments;
-        Arrange assignment;
-        //unordered_set<string>::iterator ready_queue_begin=ready_queue.
-        while ((!ready_queue.empty()) &&
-               (!available_slot.empty()))
+        while (!ready_queue.empty() &&
+               !available_slot.empty())
         {
             string task = *ready_queue.begin();
-            int DC_index = randInt(0, available_slot.size() - 1);
-            vector<pair<string, int>>::iterator iter = available_slot.begin() + DC_index;
+            int DC_index = randInt(0,
+                                   available_slot.size() - 1);
+            // vector<pair<string, int>>::iterator
+            auto iter = available_slot.begin() + DC_index;
             string DC = iter->first;
+            Arrange assignment;
             assignment.first = count_time(task, DC);
             assignment.second.first = DC;
             assignment.second.second = task;
             assignments.push_back(assignment);
-            if (!(--iter->second))
+            if (--iter->second == 0)
             {
                 available_slot.erase(iter);
             }
@@ -140,13 +149,17 @@ public:
         RANDOM
     } sched_type;
 
-    Scheduler(shared_ptr<Graph> g) : graph(g) {}
+    void initGraph(shared_ptr<Graph> graph)
+    {
+        this->graph = graph;
+    }
+
     // get new tasks from DAG
     void sumbitTasks(unordered_set<string> tasks)
     {
-        for (string each_task : tasks)
+        for (const auto &task : tasks)
         {
-            ready_queue.insert(each_task);
+            ready_queue.insert(task);
         }
         //add task into ready_queue
     }
